5394e06264867494882c3568102f02ee89838970
JusticeMax <justicenefe@gmail.com>
2018-01-13 22:32:54 +0100

    fixed .gitignore, removed ejs from package.json

--- node_modules/vue2-google-maps/src/utils/getPropsValuesMixin.js
+++
@@ -1,9 +0,0 @@
-import {mapValues} from 'lodash';
-
-export default {
-  methods: {
-    getPropsValues () {
-      return mapValues(this.$options.props, (v, k) => this[k]);
-    }
-  }
-};
--- src/components/ReportSightings.vue
+++ src/components/ReportSightings.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="content">
-    <div class="progress">
-      <!-- Come Back Here -->
-    </div>
-
     <div class="container">
+      <div class="report-progress">
+        <!-- Come Back Here -->
+      </div>
       <div class="col-md-3"></div>
       <div class="col-md-6">
         <div class="new-details">
@@ -27,11 +26,10 @@
 
 <style>
   .new-details {
-      width: 100%;
-      margin: 0 auto;
-      background: ghostwhite;
-      height: 400px;
-
+    width: 100%;
+    margin: 0 auto;
+    background: ghostwhite;
+    height: 400px;
   }
 
 </style>
