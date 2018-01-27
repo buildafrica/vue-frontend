09681fc9a6d5a07fd2505b9c11c8077cc93aa400
JusticeMax <justicenefe@gmail.com>
2018-01-28 00:21:10 +0100

    Frontpage

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -45,20 +45,62 @@
       </div>
     </div>
     <div class="latest-missing-holder">
-        <div class="latest-missing" v-for="(latestMissing, index) in missing_people" :key="index" :style="'background-image: url(' + latestMissing.picture + ');'">
-          <div class="latest-missing-label">
-            NEWEST
+      <div class="latest-missing" v-for="(latestMissing, index) in latest_missing_people" :key="index" :style="'background-image: url(' + latestMissing.picture + ');'">
+        <div class="latest-missing-label">
+          NEWEST
+        </div>
+        <div class="latest-missing-bottom-info">
+          <div class="name-and-age">
+            <h4>{{latestMissing.name}}</h4>
+            <span class="age-holder">{{latestMissing.age}}</span>
           </div>
-          <div class="latest-missing-bottom-info">
-            <div class="name-and-age">
-              <h4>{{latestMissing.name}}</h4>
-              <span class="age-holder">{{latestMissing.age}}</span>
+          <div class="latest-missing-person-location">
+            <div>Missing Since:
+              <small>{{latestMissing.missingSince}}</small>
             </div>
-            <div class="latest-missing-person-location">
-              <div>Missing Since: <small>{{latestMissing.missingSince}}</small></div>
-              <div>Last Seen: <small>{{latestMissing.lastSeen}}</small></div>
+            <div>Last Seen:
+              <small>{{latestMissing.lastSeen}}</small>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="mpa-into-continued">
+      <div class="container">
+        <div class="mpa-intro-text">
+          <h1 align="center">
+            Let's Bring Them Back!
+          </h1>
+          <p>Some missing people you will have heard of, but many more you won't. For their families, life can feel like a desperate
+            and unbearable struggle as they wait for days or even years. That's why we are heare for missing persons and
+            their familie, 24 hours a day, 365 days a year, to provide free and confidential support, help us report a sighting,
+            search our database, cross check information, query the database.</p>
+        </div>
+
+        <div class="mpa-list">
+          <div class="mpa-list-guide" v-for="(latestMissing, index) in missing_people" :key="index">
+            <div class="latest-missing" :style="'background-image: url(' + latestMissing.picture + ');'">
+              <div class="latest-missing-bottom-info">
+                <div class="name-and-age">
+                  <h4>{{latestMissing.name}}</h4>
+                  <span class="age-holder">{{latestMissing.age}}</span>
+                </div>
+                <div class="latest-missing-person-location">
+                  <div>Missing Since:
+                    <small>{{latestMissing.missingSince}}</small>
+                  </div>
+                  <div>Last Seen:
+                    <small>{{latestMissing.lastSeen}}</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div align="center">
+            <br>
+            <button type="submit" class="btn btn-default btn-colored">LOAD MORE</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -72,6 +114,31 @@
     data() {
       return {
         countryStates,
+        latest_missing_people: [{
+          picture: '/static/assets/images/man.jpg',
+          name: 'Adekunle Obi',
+          missingSince: 'Mar 05, 2015',
+          lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+          age: '22yrs',
+        }, {
+          picture: '/static/assets/images/man.jpg',
+          name: 'Adekunle Obi',
+          missingSince: 'Mar 05, 2015',
+          lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+          age: '22yrs',
+        }, {
+          picture: '/static/assets/images/man.jpg',
+          name: 'Adekunle Obi',
+          missingSince: 'Mar 05, 2015',
+          lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+          age: '22yrs',
+        }, {
+          picture: '/static/assets/images/man.jpg',
+          name: 'Adekunle Obi',
+          missingSince: 'Mar 05, 2015',
+          lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+          age: '22yrs',
+        }, ],
         missing_people: [{
             picture: '/static/assets/images/man.jpg',
             name: 'Adekunle Obi',
@@ -96,7 +163,32 @@
             missingSince: 'Mar 05, 2015',
             lastSeen: 'Adeola Odeku, Victoria Island Lagos',
             age: '22yrs',
-          }, 
+          },
+          {
+            picture: '/static/assets/images/man.jpg',
+            name: 'Adekunle Obi',
+            missingSince: 'Mar 05, 2015',
+            lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+            age: '22yrs',
+          }, {
+            picture: '/static/assets/images/man.jpg',
+            name: 'Adekunle Obi',
+            missingSince: 'Mar 05, 2015',
+            lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+            age: '22yrs',
+          }, {
+            picture: '/static/assets/images/man.jpg',
+            name: 'Adekunle Obi',
+            missingSince: 'Mar 05, 2015',
+            lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+            age: '22yrs',
+          }, {
+            picture: '/static/assets/images/man.jpg',
+            name: 'Adekunle Obi',
+            missingSince: 'Mar 05, 2015',
+            lastSeen: 'Adeola Odeku, Victoria Island Lagos',
+            age: '22yrs',
+          }
         ]
       };
     }
@@ -150,61 +242,107 @@
     background: tan;
   }
 
-.latest-missing-holder {
+  .latest-missing-holder,
+  .mpa-list {
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     padding-top: 2px;
-}
-.latest-missing {
-  height: 250px;
-  width: 100%;
-  position: relative;
-  /* background: black; */
-  margin: 0 2px;
-}
-.latest-missing:nth-child(1) {
-  margin-left: 0;
-}
-.latest-missing:nth-child(4){
-  margin-right: 0;
-}
-.latest-missing-label {
-  position: absolute;
-  /* height: 20px; */
-  width: 100px;
-  background: red;
-  color: white;
-  text-align: center;
-  padding: 10px 15px;
-}
-.latest-missing-bottom-info {
-  height: 110px;
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  background: rgba(0,0,0,0.7);
-  padding: 5px 10px;
-  color: white;
-}
-.name-and-age {
-  /* display: inline; */
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-.name-and-age h3 {
-  flex: 2;
-}
-.age-holder {
-  position: relative;
-  /* right: 0; */
-  text-align: right;
-  color:white;
-  font-size: 18px;
-  flex: 1;
-}
+  }
+
+  .latest-missing, .mpa-list-guide {
+    height: 300px;
+    width: 100%;
+    position: relative;
+    background: rgba(0, 0, 0, 0.2);
+    margin: 0 2px;
+  }
+  .mpa-list {
+    flex-wrap: wrap;
+  }
+  .mpa-list .mpa-list-guide {
+    height: 300px;
+    width: 250px;
+    margin: 10px;
+  }
+
+  .latest-missing:nth-child(1) {
+    margin-left: 0;
+  }
+
+  .latest-missing:nth-child(4) {
+    margin-right: 0;
+  }
+
+  .latest-missing-label {
+    position: absolute;
+    /* height: 20px; */
+    width: 100px;
+    background: red;
+    color: white;
+    text-align: center;
+    padding: 10px 15px;
+  }
+
+  .latest-missing-bottom-info {
+    height: 110px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    padding: 5px 10px;
+    color: white;
+  }
+
+  .name-and-age {
+    /* display: inline; */
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .name-and-age h3 {
+    flex: 2;
+  }
+
+  .age-holder {
+    position: relative;
+    /* right: 0; */
+    text-align: right;
+    color: white;
+    font-size: 18px;
+    flex: 1;
+  }
+
+  .mpa-into-continued {
+    padding: 50px 0;
+  }
+  .mpa-into-continued button {
+    color: white;
+  }
+  .mpa-into-continued button:hover {
+    color: black !important;
+    border: 1px solid black !important;
+  }
+  .mpa-intro-text {
+    padding: 50px 0 100px;
+    text-align: center;
+  }
+
+  .mpa-intro-text h1 {
+    font-size: 58px;
+  }
+
+  .mpa-intro-text p {
+    font-weight: 400;
+    font-size: 18px;
+    width: 82%;
+    margin: 0 auto;
+    position: relative;
+    color: #1c1c1c;
+  }
+
 </style>
